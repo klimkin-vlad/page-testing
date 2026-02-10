11cb24dcaa2ce5f61fe7fed4ab3d1174d2f14ffb
klimkin-vlad <97385900+klimkin-vlad@users.noreply.github.com>
2026-02-10 15:19:25 +0300

    Рефакторинг
    Разбил код по модулям.

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "This URL is not login URL"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocaltors.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocaltors.REGISTER_FORM), "Login form is not presented"
